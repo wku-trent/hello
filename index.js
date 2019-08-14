94c79229f5475c7fc339640dc474c3edc9847b00
William Ku <wku.trentglobal@gmail.com>
2019-08-14 21:37:27 +0800

    add google map and render map onselect

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const favouritesEndpoint = 'https://my-json-server.typicode.com/wku-trent/hello/favourites';
+const locations = new Object();
+let map;
 
 function onLoad() {
   loadFavorites();
@@ -28,14 +30,29 @@ function setFavourites(list) {
   const favouritesSelect = document.getElementById('favouritesSelect');
   list.map(destination => {
     const option = document.createElement('option');
-    option.value = destination.id;
+    option.value = destination.name;
     option.text = destination.name;
     favouritesSelect.add(option);
+
+    locations[destination.name] = destination.location;
   });
+  loadMap(locations[list[0].name]);
   display('favouritesSelect', true);
+  display('map', true);
   display('spinner', false);
 }
 
 function display(id, show) {
   document.getElementById(id).style.display = show ? 'block' : 'none';
+}
+
+function loadMap(location) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 11
+  });
+}
+
+function selectLocation(v) {
+  loadMap(locations[v]);
 }
